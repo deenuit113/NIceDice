4817649b293c56637f37893605b8ce3fb4449a5a
deenuit113 <leejh5314@gmail.com>
2024-04-16 19:01:54 +0900

    Feat: add scoreboard view 점수판 뷰 추가

--- nicedice/src/units/dice.presenter.tsx
+++ nicedice/src/units/dice.presenter.tsx
@@ -6,12 +6,92 @@ export default function DiceUI(props: DiceUIProps) {
     return (
         <>
             <S.Wrapper id = {props.wrapperName}>
-                <S.RollButton onClick={props.rollDice} disabled={props.isButtonDisabled}>Roll Dice</S.RollButton>
-                <S.DiceValueWrapper>
-                    {props.diceValues.map((value, index) => (
-                        <S.DiceValue key={index} diceValue={value}>{value}</S.DiceValue>
-                    ))}
-                </S.DiceValueWrapper>
+                <S.PlayerWrapper>
+                    <S.RollButton onClick={props.rollDice} disabled={props.isButtonDisabled}>Roll Dice</S.RollButton>
+                    <S.DiceValueWrapper>
+                        {props.diceValues.map((value, index) => (
+                            <S.DiceValue key={index} diceValue={value}>{value}</S.DiceValue>
+                        ))}
+                    </S.DiceValueWrapper>
+                    <S.PlayerBoardWrapper>
+                        <S.PlayerBoard>
+                            <S.BasicScoreBoard>
+                                <S.ScoreBoardTitleHeader>
+                                <tr>
+                                    <S.ScoreBoardTitle>Basic Score</S.ScoreBoardTitle>
+                                </tr>
+                                </S.ScoreBoardTitleHeader>
+                                <tbody>
+                                <tr>
+                                    <S.ScoreTitle>Aces</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Deuces</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Threes</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Fours</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Fives</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Sixes</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Subtotal</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                </tbody>
+                            </S.BasicScoreBoard>
+                            <S.SpecialScoreBoard>
+                                <S.ScoreBoardTitleHeader>
+                                <tr>
+                                    <S.ScoreBoardTitle>Special Score</S.ScoreBoardTitle>
+                                </tr>
+                                </S.ScoreBoardTitleHeader>
+                                <tbody>
+                                <tr>
+                                    <S.ScoreTitle>Choice</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>4 of a Kind</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Full House</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>S.Straight</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>L.Straight</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Yacht</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                <tr>
+                                    <S.ScoreTitle>Total Score</S.ScoreTitle>
+                                    <S.Score>0</S.Score>
+                                </tr>
+                                </tbody>
+                            </S.SpecialScoreBoard>
+                        </S.PlayerBoard>
+                    </S.PlayerBoardWrapper>
+                </S.PlayerWrapper>
             </S.Wrapper>
         </>
     );
--- nicedice/src/units/dice.styles.tsx
+++ nicedice/src/units/dice.styles.tsx
@@ -2,20 +2,30 @@ import styled from "@emotion/styled";
 
 export const Wrapper = styled.div`
     width: 100vw;
-    height: 50vw;
+    height: 48vw;
     float: right;
     padding: auto;
-    border: 3px solid red;
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     box-sizing: border-box;
+    border: 1px solid gray;
 `;
 
+export const PlayerWrapper = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    border: 3px solid red;
+`
+
 export const RollButton = styled.button`
-    width: 40%;
-    font-size: 40px;
+    width: 80%;
+    font-size: 18px;
     padding: 10px 20px 10px 20px;
     border-radius: 20px;
     :hover{
@@ -25,21 +35,20 @@ export const RollButton = styled.button`
 
 export const DiceValueWrapper = styled.div`
     background-color: #77ccff;
-    height: 100px;
-    width: 200px;
+    width: 80%;
     display: flex;
     flex-direction: row;
     border: 1px solid blue;
     text-align: center;
     align-items: center;
-    justify-content: center;
+    justify-content: space-evenly;
     margin-top: 10px;
     border-radius: 10px;
 `
 
 export const DiceValue = styled.div<{diceValue: number}>`
-    font-size: 50px;
-    margin-right: 5px;
+    font-size: 25px;
+    font-weight: bolder;
     color: ${props => {
         switch (props.diceValue) {
             case 1:
@@ -58,4 +67,59 @@ export const DiceValue = styled.div<{diceValue: number}>`
                 return 'black'; // 기본 값은 검은색으로 설정합니다.
         }
     }};
+`
+
+export const PlayerBoardWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+    height: 70%;
+    margin-top: 10px;
+    border: 1px solid blue;
+`
+
+export const PlayerBoard = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+`
+
+export const BasicScoreBoard = styled.table`
+    width: 50%;
+    height: 90%;
+    border: 1px solid black;
+    margin: 10px 10px 10px 10px;
+    padding: 10px;
+`
+
+export const SpecialScoreBoard = styled.table`
+    width: 50%;
+    height: 90%;
+    border: 1px solid black;
+    margin: 10px 10px 10px 10px;
+    padding: 10px;
+`
+
+export const ScoreBoardTitleHeader = styled.thead`
+    border: 1px solid black;
+    text-align: center;
+`
+
+export const ScoreBoardTitle = styled.th`
+    font-size: 14px;
+    font-weight: bolder;
+`
+
+export const ScoreTitle = styled.td`
+    padding: 3px 0px 3px 0px;
+    text-align: center;
+    font-weight: bold;
+    border: 1px solid gray;
+
+`
+
+export const Score = styled.td`
+    text-align: center;
+    border: 1px solid gray;
 `
