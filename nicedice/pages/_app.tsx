193450cb6eb5f6b9eab9a38aacfc0735ec165640
deenuit113 <leejh5314@gmail.com>
2024-04-16 15:58:04 +0900

    Feat: add tab title and icon

--- nicedice/pages/_app.tsx
+++ nicedice/pages/_app.tsx
@@ -1,5 +1,17 @@
-import type { AppProps } from "next/app";
+import '../styles/globals.css'
+import { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+
+    return (
+        <>
+            <Head>
+                <title>NiceDice</title>
+                <link rel="icon" href="/diceicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+        
+    );
 }
